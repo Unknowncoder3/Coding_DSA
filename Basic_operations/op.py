18c37cb9d676bf5fb1c15cf44474368f57f90ab8
Snehasish Das <snehasishdas146@gmail.com>
2025-12-21 20:39:38 +0530

    2nd

--- Basic_operations/op.py
+++ Basic_operations/op.py
@@ -1,2 +1,5 @@
+# Basic_operations/op.py
+# This program calculates the square and cube of a given number.
 n=int(input("Enter a number: "))
 print("The square of the number is:", n*n)
+print("The cube of the number is:", n*n*n)
---
+++ condtional_operators/if-else-else.py
@@ -0,0 +1,8 @@
+n=int(input("Enter a number: "))
+m=int(input("Enter another number: "))
+if n==m:
+    print("The number is same.")
+elif n>m:
+    print("The number is greater.")
+else:
+    print("The number is smaller.") 
