eab050f58201ed3b173a32a28cae657402508b47
Snehasish Das <snehasishdas146@gmail.com>
2025-12-22 18:10:16 +0530

    ternary

---
+++ arithmatic_operators/max_using_ternary.py
@@ -0,0 +1,5 @@
+# maximum of number using ternary operator
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+max_num = a if a > b else b
+print("The maximum number is:", max_num)
