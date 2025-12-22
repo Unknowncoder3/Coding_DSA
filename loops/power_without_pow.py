9c2c85d372c531a10992b154d1ba9387d730fb82
Snehasish Das <snehasishdas146@gmail.com>
2025-12-22 18:06:42 +0530

    power

---
+++ loops/power_without_pow.py
@@ -0,0 +1,7 @@
+# power without using pow() function
+base=int(input("Enter the base number: "))
+exponent=int(input("Enter the exponent: "))
+result=1
+for i in range(exponent):
+    result=result*base
+print(f"{base} raised to the power of {exponent} is:", result)
