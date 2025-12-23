d6fb1144e87c156ca3cc6e1ad6c5a22482e896b6
Snehasish Das <snehasishdas146@gmail.com>
2025-12-23 21:04:11 +0530

    string

--- strings/exc.py
+++ strings/exc.py
@@ -1,3 +1,9 @@
 s = "interview"
 print(s[100:])
 # This will not raise an error; it will simply return an empty string.
+s = "python"
+print(s[::2])
+# This will print 'pto', which are the characters at even indices.
+s = "python"
+print(s[1::2])
+# This will print 'yhn', which are the characters at odd indices.
