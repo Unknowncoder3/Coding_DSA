6ca09a4d2538d73a53533211b427e69931174aec
Snehasish Das <snehasishdas146@gmail.com>
2025-12-23 16:18:12 +0530

    string

---
+++ strings/excape_sequence.py
@@ -0,0 +1,11 @@
+#escape sequence examples
+print("Hello\tWorld")  # Tab space between Hello and World
+print("Hello\nWorld")  # New line between Hello and World
+print("He said, \"Hello World!\"")  # Double quotes inside a string
+print('It\'s a beautiful day!')  # Single quote inside a string
+print("C:\\Users\\Name")  # Backslash in a file path
+print("First Line\rSecond Line")  # Carriage return
+print("Column1\bColumn2")  # Backspace between Column1 and Column2
+print("This is a form feed\fexample.")  # Form feed example
+print("Unicode character: \u03A9")  # Greek capital letter Omega
+print("Octal value: \101")  # Octal value for 'A'
