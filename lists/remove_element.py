e07356b2f3d6af9e0a1cb0490fc510f2fd3c4800
Snehasish Das <snehasishdas146@gmail.com>
2025-12-24 02:11:29 +0530

    list

---
+++ lists/remove_element.py
@@ -0,0 +1,7 @@
+#removing a element in list using remove() method
+mylist=[10,20,30,40,50]
+print("Original list:", mylist)
+mylist.remove(30)
+print("List after removing 30:", mylist)
+mylist.remove(10)
+print("List after removing 10:", mylist)
