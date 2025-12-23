780cbc4a662931193843b0efd56011e644c6c452
Snehasish Das <snehasishdas146@gmail.com>
2025-12-23 15:54:48 +0530

    string

---
+++ strings/exc5.py
@@ -0,0 +1,3 @@
+s = "abcdef"
+print(s[-2::-1])
+# This will print 'edcba' which is the substring from index -2 to the start in reverse order.
---
+++ strings/reverse_using_slicing.py
@@ -0,0 +1,4 @@
+#Reverse the string using slicing.
+n=str(input("Enter a string: "))
+reversed_string=n[::-1]
+print("Reversed string is:", reversed_string)
