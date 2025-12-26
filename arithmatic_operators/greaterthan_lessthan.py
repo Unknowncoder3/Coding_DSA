5fc6b87c0c3018f7d43eb4174fc5b4bc037ea25a
Snehasish Das <snehasishdas146@gmail.com>
2025-12-26 23:07:00 +0530

    new

---
+++ arithmatic_operators/greaterthan_lessthan.py
@@ -0,0 +1,11 @@
+#greater than and less than operators
+a = 10
+b = 20  
+print("a > b:", a > b)  # False
+print("a < b:", a < b)  # True
+print("b > a:", b > a)  # True
+print("b < a:", b < a)  # False
+print("a > 5:", a > 5)  # True
+print("b < 25:", b < 25)  # True
+print("a < 10:", a < 10)  # False
+print("b > 20:", b > 20)  # False
