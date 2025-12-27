f8a3027399007b12959693f7cb7ce9b6184b3caf
Snehasish Das <snehasishdas146@gmail.com>
2025-12-27 15:55:07 +0530

    algo

--- algorithms/tower_of_hanoi.py
+++ algorithms/tower_of_hanoi.py
@@ -1,4 +1,4 @@
-#tower of hanoi
+#tower of hanoi using recursion using userdefined function
 def tower_of_hanoi(n, source, target, auxiliary):
     if n == 1:
         print(f"Move disk 1 from {source} to {target}")
@@ -6,4 +6,8 @@ def tower_of_hanoi(n, source, target, auxiliary):
     tower_of_hanoi(n - 1, source, auxiliary, target)
     print(f"Move disk {n} from {source} to {target}")
     tower_of_hanoi(n - 1, auxiliary, target, source)
+num_disks = int(input("Enter the number of disks: "))
+tower_of_hanoi(num_disks, 'A', 'C', 'B')
+
+
 
