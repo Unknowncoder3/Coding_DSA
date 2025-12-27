7e922ff021d7676cafa39a7a0bafcd2a27df715f
Snehasish Das <snehasishdas146@gmail.com>
2025-12-27 16:09:56 +0530

    algo

---
+++ algorithms/krushkals_algo.py
@@ -0,0 +1,45 @@
+#krushkals algorithm using userdefined function
+class DisjointSet:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, u):
+        if self.parent[u] != u:
+            self.parent[u] = self.find(self.parent[u])
+        return self.parent[u]
+
+    def union(self, u, v):
+        root_u = self.find(u)
+        root_v = self.find(v)
+        if root_u != root_v:
+            if self.rank[root_u] > self.rank[root_v]:
+                self.parent[root_v] = root_u
+            elif self.rank[root_u] < self.rank[root_v]:
+                self.parent[root_u] = root_v
+            else:
+                self.parent[root_v] = root_u
+                self.rank[root_u] += 1
+def kruskal(n, edges):
+    edges.sort(key=lambda x: x[2])
+    ds = DisjointSet(n)
+    mst_weight = 0
+    mst_edges = []
+    for u, v, weight in edges:
+        if ds.find(u) != ds.find(v):
+            ds.union(u, v)
+            mst_weight += weight
+            mst_edges.append((u, v, weight))
+    return mst_weight, mst_edges
+# Example usage
+n = int(input("Enter number of vertices: "))
+m = int(input("Enter number of edges: "))
+edges = []
+for _ in range(m):
+    u, v, weight = map(int, input("Enter edge (u v weight): ").split())
+    edges.append((u, v, weight))
+mst_weight, mst_edges = kruskal(n, edges)
+print("Weight of MST:", mst_weight)
+print("Edges in the MST:")
+for u, v, weight in mst_edges:
+    print(f"{u} -- {v} == {weight}")
