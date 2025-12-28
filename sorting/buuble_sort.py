f43adb6445abcc26a7ca07dc77c69230688cbbe0
Snehasish Das <snehasishdas146@gmail.com>
2025-12-28 20:28:20 +0530

    sort

---
+++ sorting/buuble_sort.py
@@ -0,0 +1,11 @@
+#Bubble sort using user defined function
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+arr = list(map(int, input("Enter elements of the array separated by spaces: ").split()))
+sorted_arr = bubble_sort(arr)
+print("Sorted array:", sorted_arr)
