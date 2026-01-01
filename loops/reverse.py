cd5bd56a9539f5940d92b4f15c61fba00f58d7dd
Snehasish Das <snehasishdas146@gmail.com>
2026-01-01 09:34:57 +0530

    first

--- loops/reverse.py
+++ loops/reverse.py
@@ -1,2 +1,17 @@
-#reverse a list using loop
+#Reverse a number using loop
+num=int(input("Enter a number to reverse: "))
+reversed_num=0
+while(num>0):
+    digit=num%10
+    reversed_num=reversed_num*10+digit
+    num=num//10
+print("Reversed Number: ",reversed_num)
+
+#Reverse a string using loop
+string=input("Enter a string to reverse: ")
+reversed_string=""
+for i in range(len(string)-1,-1,-1):
+    reversed_string+=string[i]
+print("Reversed String: ",reversed_string)
+
 
