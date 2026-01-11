6f4de7e729f0679cf2d8e0e441dd97becc37e99d
Snehasish Das <snehasishdas146@gmail.com>
2026-01-12 00:20:57 +0530

    gfg

---
+++ GFG_POTD/12thjan2026.py
@@ -0,0 +1,24 @@
+# MAXIMUM OF ALL SUBARRAYS OF SIZE K
+from collections import deque
+
+class Solution:
+    def maxOfSubarrays(self, arr, k):
+        dq = deque()
+        res = []
+        
+        for i in range(len(arr)):
+            # Remove elements out of this window
+            while dq and dq[0] <= i - k:
+                dq.popleft()
+            
+            # Remove smaller elements from the back
+            while dq and arr[dq[-1]] <= arr[i]:
+                dq.pop()
+            
+            dq.append(i)
+            
+            # First window completed
+            if i >= k - 1:
+                res.append(arr[dq[0]])
+        
+        return res
