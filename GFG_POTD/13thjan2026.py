f92312e5dca23e5d11fec330f23498b89c0bc94b
Snehasish Das <snehasishdas146@gmail.com>
2026-01-13 00:12:28 +0530

    gfg

---
+++ GFG_POTD/13thjan2026.py
@@ -0,0 +1,26 @@
+#bus ticket change
+class Solution:
+    def canServe(self, arr):
+        five = 0
+        ten = 0
+        
+        for x in arr:
+            if x == 5:
+                five += 1
+            
+            elif x == 10:
+                if five == 0:
+                    return False
+                five -= 1
+                ten += 1
+            
+            else:  # x == 20
+                if ten > 0 and five > 0:
+                    ten -= 1
+                    five -= 1
+                elif five >= 3:
+                    five -= 3
+                else:
+                    return False
+        
+        return True
