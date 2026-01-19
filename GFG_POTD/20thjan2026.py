ad56194ebce7c5dd5c2bd1522a4204ce8b95ccfb
Snehasish Das <snehasishdas146@gmail.com>
2026-01-20 00:54:58 +0530

    gfg

---
+++ GFG_POTD/20thjan2026.py
@@ -0,0 +1,25 @@
+# Implement a simple text editor with append, undo, redo, and read operations.
+class Solution:
+    def __init__(self):
+        self.doc = []
+        self.undo_stack = []
+        self.redo_stack = []
+
+    def append(self, x):
+        self.doc.append(x)
+        self.undo_stack.append(x)
+        self.redo_stack.clear()
+
+    def undo(self):
+        if self.doc:
+            ch = self.doc.pop()
+            self.redo_stack.append(ch)
+
+    def redo(self):
+        if self.redo_stack:
+            ch = self.redo_stack.pop()
+            self.doc.append(ch)
+            self.undo_stack.append(ch)
+
+    def read(self):
+        return "".join(self.doc)
