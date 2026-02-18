4f3c7128596d74e5ea55f2446a468c2e1ebaa653
Snehasish Das <snehasishdas146@gmail.com>
2026-02-18 10:20:01 +0530

    gfg

---
+++ GFG_POTD/18thfeb2026.py
@@ -0,0 +1,48 @@
+# Count Inversions
+class Solution:
+    def inversionCount(self, arr):
+        def merge_sort(arr, temp, left, right):
+            inv_count = 0
+            if left < right:
+                mid = (left + right) // 2
+
+                inv_count += merge_sort(arr, temp, left, mid)
+                inv_count += merge_sort(arr, temp, mid + 1, right)
+                inv_count += merge(arr, temp, left, mid, right)
+
+            return inv_count
+
+        def merge(arr, temp, left, mid, right):
+            i = left       # left subarray pointer
+            j = mid + 1    # right subarray pointer
+            k = left       # temp array pointer
+            inv_count = 0
+
+            while i <= mid and j <= right:
+                if arr[i] <= arr[j]:
+                    temp[k] = arr[i]
+                    i += 1
+                else:
+                    temp[k] = arr[j]
+                    inv_count += (mid - i + 1)
+                    j += 1
+                k += 1
+
+            while i <= mid:
+                temp[k] = arr[i]
+                i += 1
+                k += 1
+
+            while j <= right:
+                temp[k] = arr[j]
+                j += 1
+                k += 1
+
+            for idx in range(left, right + 1):
+                arr[idx] = temp[idx]
+
+            return inv_count
+
+        n = len(arr)
+        temp = [0] * n
+        return merge_sort(arr, temp, 0, n - 1)
