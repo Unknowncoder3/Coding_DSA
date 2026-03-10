28c9a7a534bf188cb9563a00611c82d8e45ccc8f
Snehasish Das <snehasishdas146@gmail.com>
2026-03-10 20:55:04 +0530

    gfg

---
+++ GFG_POTD/10mar2026.py
@@ -0,0 +1,18 @@
+# Given an array of integers, count the number of subarrays where the minimum element is the first element of the subarray.
+class Solution:
+    def countSubarrays(self, arr):
+        n = len(arr)
+        stack = []
+        next_smaller = [n] * n
+
+        for i in range(n):
+            while stack and arr[stack[-1]] > arr[i]:
+                idx = stack.pop()
+                next_smaller[idx] = i
+            stack.append(i)
+
+        ans = 0
+        for i in range(n):
+            ans += next_smaller[i] - i
+
+        return ans
